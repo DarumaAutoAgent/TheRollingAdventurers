8e9d921f455e8d339eb33e57898f2ad7350dee04
QDaruma <daruma.auto.agent@gmail.com>
2026-04-07 15:04:33 +0200

    Force absolute asset paths for CSS loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: '/',
+  trailingSlash: 'always',
+  build: {
+    assetsPrefix: '/'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
